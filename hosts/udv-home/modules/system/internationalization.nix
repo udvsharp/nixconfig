1c1b14e3223aa13b81c7d7ec4c24c0faee9a32dd
Dmitry Vasyliev <dmitry.vasyliev@protonmail.com>
2024-08-11 16:27:55 +0300

    Resolved internationalization problems

--- hosts/udv-home/modules/system/internationalization.nix
+++ hosts/udv-home/modules/system/internationalization.nix
@@ -6,7 +6,7 @@
         ruRU = "ru_RU.UTF-8/UTF-8";
         ukUA = "uk_UA.UTF-8/UTF-8";
     in {
-        defaultLocale = enUS;
+        defaultLocale = "en_US.UTF-8"; # Doesn't work with postfix for some reason
         supportedLocales = [
             enUS
             ruRU
