27715e2f1aad6cbd5c8df3867ec6b1005ea47dc9
Dmitry Vasyliev <dmitry.vasyliev@protonmail.com>
2024-08-13 01:40:08 +0300

    Expanded git configuration

--- hosts/udv-home/users/udv/modules/git.nix
+++ hosts/udv-home/users/udv/modules/git.nix
@@ -5,5 +5,40 @@
     programs.git = {
         userName = "Dmitry Vasyliev";
         userEmail = "dmitry.vasyliev@protonmail.com";
+
+        aliases = {
+            # LogGraph - Pretty graph log
+            lg = "log --graph --abbrev-commit --decorate --all";
+            # STatusShort - short version of status
+            sts = "status --short --branch --renames --ahead-behind";
+            # Last commit info - detailed (with diff)
+            last = "log -1 HEAD --stat";
+            # Verbose remote list - with links
+            rv = "remote --verbose";
+        };
+
+        # TODO: migrate to vim for that :)
+        extraConfig = {
+            core = {
+                editor = "code --wait";
+            };
+            merge = {
+                tool = "vscode";
+            };
+            diff = {
+                tool = "vscode";
+            };
+            mergetool = {
+                vscode = {
+                    cmd = "code --wait $MERGED";
+                    trustExitCode = true;
+                };
+            };
+            difftool = {
+                vscode = {
+                    cmd = "code --wait --diff $LOCAL $REMOTE";
+                };
+            };
+        };
     };
 }
