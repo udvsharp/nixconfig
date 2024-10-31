acce72c68f09919ada003275e10aba8c1dc30381
Dmitry Vasyliev <dmitry.vasyliev@protonmail.com>
2024-10-31 21:28:54 +0200

    Modified telescope remaps

--- packages/nvim/lua/udv/options/remaps/telescope.lua
+++ packages/nvim/lua/udv/options/remaps/telescope.lua
@@ -2,4 +2,7 @@
 local builtin = require("telescope.builtin")
 
 -- TODO: udv.keymap_leader
-vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pv', builtin.git_files, {})    -- Project - VCS Files
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})   -- Project - Files
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})    -- Project - Grep
+
